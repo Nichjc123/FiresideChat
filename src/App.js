8979a8247703adef5824907860b08d54e6d40922
Nichjc123 <nichjc123@gmail.com>
2022-10-24 22:19:44 -0400

    very basic flow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
 import "./styles.css";
+import { useState } from "react";
+//Component imports
+import Header from "./components/Header";
+import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
 
 function App() {
-  return <div className="App">YO</div>;
+  const [userStatus, setUserStatus] = useState("def");
+
+  const BackButton = () => {
+    return (
+      <button
+        onClick={() => {
+          setUserStatus("def");
+        }}
+      >
+        Back -&gt;
+      </button>
+    );
+  };
+
+  const MainContent = () => {
+    if (userStatus === "signUp") {
+      return (
+        <div className="flex-cent col">
+          <SignUpForm />
+          <BackButton />
+        </div>
+      );
+    } else if (userStatus === "signIn") {
+      return (
+        <div className="flex-cent col">
+          <SignInForm />
+          <BackButton />
+        </div>
+      );
+    } else if (userStatus === "def") {
+      return (
+        <div className="flex-cent">
+          <button
+            onClick={() => {
+              setUserStatus("signUp");
+            }}
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={() => {
+              setUserStatus("signIn");
+            }}
+          >
+            Sign In
+          </button>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="flx-cent col">
+      <Header />
+      <MainContent />
+    </div>
+  );
 }
 
 export default App;
+
+//this will dynamically render either sign up/sign in/chatroom
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex-cent col">
+      {/* Have a background pic of a fire burning gif type thing */}
+      <h2>Fireside Chat</h2>
+      <p>Come take seat.</p>
+    </div>
+  );
+}
+
+export default Header;
